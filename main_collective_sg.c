f643155837ca44dfa3fa15628bc0e9f41b699575
Sameer Pande <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2020-02-27 23:01:05 +0530

    almost working but gives error for 2 workers only, need to debug

--- main_collective_sg.c
+++ main_collective_sg.c
@@ -43,16 +43,144 @@ void printMatrix(float * matrix,int nrows,int ncols){
 int main(int argc, char**argv){
 
     int rank,numProcesses;
+    int aRowsActual;
+    int aRows,aCols,bCols;
+
+    float* A,*B,*C;
     MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
     MPI_Comm_size(MPI_COMM_WORLD,&numProcesses);
-    
+    int numWorkers = numProcesses - 1;
     if(numProcesses<=1){
       printf("Number of Processes should be greater than 1\n");
       MPI_Finalize();
       return 0;
     }
+    
+    if(rank==0){
+
+        printf("Collective \n");
+        double beginTime = MPI_Wtime();
+       
+        int aRowsActual = atoi(argv[1]);
+
+        if( aRowsActual % numWorkers == 0 ){
+            aRows = aRowsActual;
+        }
+        else{
+            aRows = (aRowsActual/numWorkers + 1)*numWorkers; 
+        }
+
+        printf("%d aRows\n",aRows);
+
+
+        aCols = atoi(argv[2]);
+        printf("%d aCols\n",aCols);
+        
+        
+        int bRows = aCols;
+        bCols = atoi(argv[3]);
+        printf("%d bCols\n",bCols);
+        
+        
+        int cRows = aRows;
+        int cCols = bCols;
+
+        
+
+        int aRowSize = aCols;
+        int aColSize = aRows;
+        int bRowSize = bCols;
+        int bColSize = bRows;
+        int cRowSize = bCols;
+        int cColSize = aRows;
+
+        A = (float *)malloc(sizeof(float)*aRows*aCols);
+        B = (float *)malloc(sizeof(float)*bRows*bCols);
+        C = (float *)malloc(sizeof(float)*aRows*bCols);
+        
+        srand48(time(0));
+        for(int i =0 ;i<aRows*aCols;i++){
+          A[i] = ((float)rand()) / ((float)(RAND_MAX));
+          if(i>=aRowsActual*aCols){
+              A[i]=0;
+          }
+        }
+        for(int i =0 ;i<bRows*bCols;i++){
+          B[i] = ((float)rand()) / ((float)(RAND_MAX));
+        }
+
+        // MPI_Bcast(&aRows,1,MPI_INT,0,MPI_COMM_WORLD);
+        // MPI_Bcast(&aCols,1,MPI_INT,0,MPI_COMM_WORLD);
+        // MPI_Bcast(&bCols,1,MPI_INT,0,MPI_COMM_WORLD);
+        // MPI_Bcast(B,aCols*bCols,MPI_FLOAT,0,MPI_COMM_WORLD);
+
+    }
+
+
+    MPI_Bcast(&aRows,1,MPI_INT,0,MPI_COMM_WORLD);
+    MPI_Bcast(&aCols,1,MPI_INT,0,MPI_COMM_WORLD);
+    MPI_Bcast(&bCols,1,MPI_INT,0,MPI_COMM_WORLD);
+    
+    MPI_Barrier(MPI_COMM_WORLD);//ensuring that all the values have been updated
+
+    // printf("aCols = %d, bCols = %d\n",aCols,bCols);   
+    
+    MPI_Barrier(MPI_COMM_WORLD);
+    // printf("rank = %d, aRows = %d, aCols = %d, bCols = %d\n",rank,aRows,aCols,bCols);
+   
+    float * A_part = (float*)malloc(aRows/numWorkers * aCols * sizeof(float));
+    float * C_part = (float*)malloc(aRows/numWorkers * bCols * sizeof(float));
+    B = (float *)malloc(aCols * bCols *sizeof(float)); 
+    C = (float *)malloc(aRows * bCols * sizeof(float));
+
+    MPI_Bcast(B,aCols * bCols,MPI_FLOAT,0,MPI_COMM_WORLD);
+    MPI_Scatter(A, aRows/numWorkers * aCols, MPI_FLOAT, A_part, aRows/numWorkers * aCols, MPI_FLOAT,0,MPI_COMM_WORLD);
+
+    int rowsReceived = aRows/numWorkers;
+    printf("rank = %d,  rowsReceived = %d\n",rank,rowsReceived);
+
+    for(int i = 0;i<rowsReceived;i++){
+        for(int j = 0;j<bCols;j++){
+            C_part[ i * bCols + j] = 0;
+
+            for(int k = 0;k<aCols;k++){
+                C_part[i*bCols + j] += ( A_part[i*aCols + k] *B[k*bCols+j]);
+            }
+        }
+    }
+
+    MPI_Gather(C_part, aRows/numWorkers * bCols , MPI_FLOAT, C, aRows/numWorkers * bCols, MPI_FLOAT,0,MPI_COMM_WORLD );
+    
+    MPI_Barrier(MPI_COMM_WORLD);
+
 
     
+
+
+
+
+    
+    if(rank == 0){
+
+        // float * C_actual;
+        // if(aRowsActual !=aRows){
+        //     C_actual = (float *)malloc( aRowsActual * bCols * sizeof(float));
+        //     for(int i = 0;i<aRowsActual*bCols;i++)
+        //         C_actual[i] = C[i];
+        // }
+        // else{
+        //     C_actual = C;
+        // }
+
+        float * C_serial = (float *)malloc(sizeof(float)*aRows*bCols);
+        Multiply_serial(A,B,C_serial,aRows,aCols,bCols);
+        // printMatrix(C,aRows,bCols);
+        // printMatrix(C_serial,aRows,bCols);
+        printf("%d \n",IsEqual(C_serial,C,aRows,bCols));
+
+    }
+
+
     MPI_Finalize();
 }
--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 numWorkers=`expr $2 + 1`
 
-mpicc main_nonblocking.c -o main_nonblocking.exe
-mpirun -np $numWorkers ./main_nonblocking.exe $1 32 $1
+# mpicc main_nonblocking.c -o main_nonblocking.exe
+# mpirun -np $numWorkers ./main_nonblocking.exe $1 32 $1
 
-mpicc main_blocking.c -o main_blocking.exe
-mpirun -np $numWorkers ./main_blocking.exe $1 32 $1
+# mpicc main_blocking.c -o main_blocking.exe
+# mpirun -np $numWorkers ./main_blocking.exe $1 32 $1
 
-mpicc main_collective.c -o main_collective.exe
-mpirun -np $numWorkers ./main_collective.exe $1 32 $1
+# mpicc main_collective.c -o main_collective.exe
+# mpirun -np $numWorkers ./main_collective.exe $1 32 $1
+
+
+mpicc main_collective_sg.c -o main_collective_sg.exe
+mpirun -np $numWorkers ./main_collective_sg.exe $1 32 $1
