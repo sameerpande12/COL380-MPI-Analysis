01b9eb32f10430b7b08450aa4b8fb118d08a1619
Sameer Pande <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2020-02-28 19:46:42 +0530

    new data

--- generateData.sh
+++ generateData.sh
@@ -6,6 +6,8 @@ bash run.sh 800 2  > Data/800_2.txt
 bash run.sh 1600 2 > Data/1600_2.txt
 bash run.sh 3200 2 > Data/3200_2.txt
 bash run.sh 6400 2 > Data/6400_2.txt
+bash run.sh 8000 2 > Data/8000_2.txt
+bash run.sh 10000 2 > Data/10000_2.txt
 bash run.sh 12800 2 > Data/12800_2.txt
 
 
@@ -16,6 +18,8 @@ bash run.sh 800 4  > Data/800_4.txt
 bash run.sh 1600 4 > Data/1600_4.txt
 bash run.sh 3200 4 > Data/3200_4.txt
 bash run.sh 6400 4 > Data/6400_4.txt
+bash run.sh 8000 4 > Data/8000_4.txt
+bash run.sh 10000 4 > Data/10000_4.txt
 bash run.sh 12800 4 > Data/12800_4.txt
 
 bash run.sh 100 6  > Data/100_6.txt
@@ -25,6 +29,8 @@ bash run.sh 800 6  > Data/800_6.txt
 bash run.sh 1600 6 > Data/1600_6.txt
 bash run.sh 3200 6 > Data/3200_6.txt
 bash run.sh 6400 6 > Data/6400_6.txt
+bash run.sh 8000 6 > Data/8000_6.txt
+bash run.sh 10000 6 > Data/10000_6.txt
 bash run.sh 12800 6 > Data/12800_6.txt
 
 
@@ -35,6 +41,8 @@ bash run.sh 800 8  > Data/800_8.txt
 bash run.sh 1600 8 > Data/1600_8.txt
 bash run.sh 3200 8 > Data/3200_8.txt
 bash run.sh 6400 8 > Data/6400_8.txt
+bash run.sh 8000 8 > Data/8000_8.txt
+bash run.sh 10000 8 > Data/10000_8.txt
 bash run.sh 12800 8 > Data/12800_8.txt
 
 
@@ -45,6 +53,8 @@ bash run.sh 800 12 > Data/800_12.txt
 bash run.sh 1600 12 > Data/1600_12.txt
 bash run.sh 3200 12 > Data/3200_12.txt
 bash run.sh 6400 12 > Data/6400_12.txt
+bash run.sh 8000 12 > Data/8000_12.txt
+bash run.sh 10000 12 > Data/10000_12.txt
 bash run.sh 12800 12 > Data/12800_12.txt
 
 bash run.sh 100 16  > Data/100_16.txt
@@ -54,19 +64,6 @@ bash run.sh 800 16 > Data/800_16.txt
 bash run.sh 1600 16 > Data/1600_16.txt
 bash run.sh 3200 16 > Data/3200_16.txt
 bash run.sh 6400 16 > Data/6400_16.txt
-bash run.sh 12800 16 > Data/12800_16.txt
-
-
-bash run.sh 8000 2 > Data/8000_2.txt
-bash run.sh 8000 4 > Data/8000_4.txt
-bash run.sh 8000 6 > Data/8000_6.txt
-bash run.sh 8000 8 > Data/8000_8.txt
-bash run.sh 8000 12 > Data/8000_12.txt
 bash run.sh 8000 16 > Data/8000_16.txt
-
-bash run.sh 10000 2 > Data/10000_2.txt
-bash run.sh 10000 4 > Data/10000_4.txt
-bash run.sh 10000 6 > Data/10000_6.txt
-bash run.sh 10000 8 > Data/10000_8.txt
-bash run.sh 10000 12 > Data/10000_12.txt
 bash run.sh 10000 16 > Data/10000_16.txt
+bash run.sh 12800 16 > Data/12800_16.txt
