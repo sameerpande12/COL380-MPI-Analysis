0499b9fd4ca88619a96e4f9edd30288dca655433
Sameer Pande <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2020-02-28 18:29:03 +0530

    updated serial data

---
+++ generateSerialData.sh
@@ -0,0 +1,18 @@
+bash run_serial.sh 100 > Data/serial_100.txt
+bash run_serial.sh 200 > Data/serial_200.txt
+bash run_serial.sh 400 > Data/serial_400.txt
+bash run_serial.sh 800 > Data/serial_800.txt
+bash run_serial.sh 1600 > Data/serial_1600.txt
+bash run_serial.sh 3200 > Data/serial_3200.txt
+bash run_serial.sh 6400 > Data/serial_6400.txt
+bash run_serial.sh 12800 > Data/serial_12800.txt
+
+
+bash run_serial.sh 100 >> Data/serial_100.txt
+bash run_serial.sh 200 >> Data/serial_200.txt
+bash run_serial.sh 400 >> Data/serial_400.txt
+bash run_serial.sh 800 >> Data/serial_800.txt
+bash run_serial.sh 1600 >> Data/serial_1600.txt
+bash run_serial.sh 3200 >> Data/serial_3200.txt
+bash run_serial.sh 6400 >> Data/serial_6400.txt
+bash run_serial.sh 12800 >> Data/serial_12800.txt
