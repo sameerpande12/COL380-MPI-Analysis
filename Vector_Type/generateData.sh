b7b8ca22e4ad6bcf2caaa820e1b062e618a9b521
Sameer Pande <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2020-02-28 20:13:13 +0530

    about to generate cpp code in this new folder

---
+++ Vector_Type/generateData.sh
@@ -0,0 +1,69 @@
+mkdir -p Data
+bash run.sh 100 2  > Data/100_2.txt
+bash run.sh 200 2  > Data/200_2.txt
+bash run.sh 400 2  > Data/400_2.txt
+bash run.sh 800 2  > Data/800_2.txt
+bash run.sh 1600 2 > Data/1600_2.txt
+bash run.sh 3200 2 > Data/3200_2.txt
+bash run.sh 6400 2 > Data/6400_2.txt
+bash run.sh 8000 2 > Data/8000_2.txt
+bash run.sh 10000 2 > Data/10000_2.txt
+bash run.sh 12800 2 > Data/12800_2.txt
+
+
+bash run.sh 100 4  > Data/100_4.txt
+bash run.sh 200 4  > Data/200_4.txt
+bash run.sh 400 4  > Data/400_4.txt
+bash run.sh 800 4  > Data/800_4.txt
+bash run.sh 1600 4 > Data/1600_4.txt
+bash run.sh 3200 4 > Data/3200_4.txt
+bash run.sh 6400 4 > Data/6400_4.txt
+bash run.sh 8000 4 > Data/8000_4.txt
+bash run.sh 10000 4 > Data/10000_4.txt
+bash run.sh 12800 4 > Data/12800_4.txt
+
+bash run.sh 100 6  > Data/100_6.txt
+bash run.sh 200 6  > Data/200_6.txt
+bash run.sh 400 6  > Data/400_6.txt
+bash run.sh 800 6  > Data/800_6.txt
+bash run.sh 1600 6 > Data/1600_6.txt
+bash run.sh 3200 6 > Data/3200_6.txt
+bash run.sh 6400 6 > Data/6400_6.txt
+bash run.sh 8000 6 > Data/8000_6.txt
+bash run.sh 10000 6 > Data/10000_6.txt
+bash run.sh 12800 6 > Data/12800_6.txt
+
+
+bash run.sh 100 8  > Data/100_8.txt
+bash run.sh 200 8  > Data/200_8.txt
+bash run.sh 400 8  > Data/400_8.txt
+bash run.sh 800 8  > Data/800_8.txt
+bash run.sh 1600 8 > Data/1600_8.txt
+bash run.sh 3200 8 > Data/3200_8.txt
+bash run.sh 6400 8 > Data/6400_8.txt
+bash run.sh 8000 8 > Data/8000_8.txt
+bash run.sh 10000 8 > Data/10000_8.txt
+bash run.sh 12800 8 > Data/12800_8.txt
+
+
+bash run.sh 100 12  > Data/100_12.txt
+bash run.sh 200 12  > Data/200_12.txt
+bash run.sh 400 12  > Data/400_12.txt
+bash run.sh 800 12 > Data/800_12.txt
+bash run.sh 1600 12 > Data/1600_12.txt
+bash run.sh 3200 12 > Data/3200_12.txt
+bash run.sh 6400 12 > Data/6400_12.txt
+bash run.sh 8000 12 > Data/8000_12.txt
+bash run.sh 10000 12 > Data/10000_12.txt
+bash run.sh 12800 12 > Data/12800_12.txt
+
+bash run.sh 100 16  > Data/100_16.txt
+bash run.sh 200 16  > Data/200_16.txt
+bash run.sh 400 16  > Data/400_16.txt
+bash run.sh 800 16 > Data/800_16.txt
+bash run.sh 1600 16 > Data/1600_16.txt
+bash run.sh 3200 16 > Data/3200_16.txt
+bash run.sh 6400 16 > Data/6400_16.txt
+bash run.sh 8000 16 > Data/8000_16.txt
+bash run.sh 10000 16 > Data/10000_16.txt
+bash run.sh 12800 16 > Data/12800_16.txt
